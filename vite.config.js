d7f29b018f45918daa0e0ef22f483a898905806c
shirwani <zakishirwani@alumni.purdue.edu>
2025-03-30 22:01:20 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import mkcert from "vite-plugin-mkcert";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), mkcert()],
+  base: '/Programmatic.io/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
